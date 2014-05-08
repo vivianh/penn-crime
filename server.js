9fc8bf95d39d6f6ff55f4e9603c8e03f4c18625b
vivian <vivianhhuang@gmail.com>
2014-05-08 00:01:24 -0400

    combining requests for now

--- server.js
+++ server.js
@@ -4,54 +4,24 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
 
-app.get('/scrape/:week', function (req, res) {
-
-  url = 'http://www.upenn.edu/almanac/volumes/v60/n01/creport.html'
-
-  request(url, function (err, res, html) {
-    if (!err) {
-      $ = cheerio.load(html);
-      
-      var kids = []
-      $('table[border=1]').each(function (i, e) {
-        if ($(e).get('0').attribs.cellpadding === '3') {
-          // console.log($(e).children().length);
-          kids.push($(e).children());
-        }
-      });
-
-      var crimes = []
-      $(kids).each(function (i, e) {
-        $(e).each(function(j, x) {
-          var date, time, loc, type;
-          var json = { date : "", time : "", loc : "", type : "" };
-          json.date = $(x).children().first().find('span').text();
-          json.time = $(x).children().next().first().find('span').text();
-          json.loc  = $(x).children().next().next().first().find('span').text();
-          json.type = $(x).children().next().next().next().first().find('span').text();
-          // console.log(json);
-          crimes.push(json);
-        });
-      });
-
-    }
-  });
-
-});
-
 app.get('/scrape', function (req, res) {
-  url = 'http://www.upenn.edu/almanac/crimes-index.html'
+
+  var url = 'http://www.upenn.edu/almanac/crimes-index.html'
+  this.crimes = [];
+  var self = this;
 
   request(url, function (err, res, html) {
     if (!err) {
-      $ = cheerio.load(html);
 
       var links = [];
+
+      $ = cheerio.load(html);
+
       $('table[border=0]').each(function (i, e) {
         if ($(e).get('0').attribs.cellpadding === '10') {
           var anchors = $(e).find('a');
+
           $(anchors).each(function(j, x) {
-            // console.log($(x).attr('href'));
             var link = $(x).attr('href');
             if (link.substring(0, 4) !== 'http') {
               link = 'http://www.upenn.edu/almanac/'.concat(link);
@@ -62,11 +32,43 @@ app.get('/scrape', function (req, res) {
       });
 
       links.forEach(function (link) {
+        request(link, function (err, res, html) {
+          if (!err) {
+
+            var kids = [];
+
+            $ = cheerio.load(html);
+
+            $('table[border=1]').each(function (i, e) {
+              if ($(e).get('0').attribs.cellpadding === '3') {
+                kids.push($(e).children());
+              }
+            });
+
+            $(kids).each(function (i, e) {
+              $(e).each(function(j, x) {
+                var date, time, loc, type;
+                var json = { date : "", time : "", loc : "", type : "" };
+                json.date = $(x).children().first().find('span').text();
+                if (json.date.length === 0) {
+                  console.log(i, link);
+                }
+                json.time = $(x).children().next().first().find('span').text();
+                json.loc  = $(x).children().next().next().first().find('span').text();
+                json.type = $(x).children().next().next().next().first().find('span').text();
+                self.crimes.push(json);
+              });
+            });
+
+          }
+        });
+
       });
     }
   });
-  
+
 });
+
 
 app.listen('3000');
 
