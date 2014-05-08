d09757a9c8651e5c488b5182c65a7a49299c9fba
vivian <vivianhhuang@gmail.com>
2014-05-08 10:40:16 -0400

    properly creates json output

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var _ = require('underscore');
 var app = express();
 
 app.get('/scrape', function (req, res) {
+  // 'use strict';
 
-  var url = 'http://www.upenn.edu/almanac/crimes-index.html'
-  this.crimes = [];
+  var url = 'http://www.upenn.edu/almanac/crimes-index.html';
   var self = this;
+  self.check = true;
+  self.crimes = [];
 
-  request(url, function (err, res, html) {
+  request(url, function (err, resp, html) {
     if (!err) {
 
       var links = [];
@@ -33,13 +36,17 @@ app.get('/scrape', function (req, res) {
 
       // links = ['http://www.upenn.edu/almanac/volumes/v60/n33/creport.html'];
 
-      links.forEach(function (link) {
-        request(link, function (err, res, html) {
-          if (!err) {
+      var finished = _.after(links.length, writeToFile);
+
+      links.forEach(function (_link) {
+        request(_link, function (_err, _res, _html) {
+          // console.log(_link);
+          // console.log('err', _err);
+          if (!_err) {
 
             var kids = [];
 
-            $ = cheerio.load(html);
+            $ = cheerio.load(_html);
 
             $('table[border=1]').each(function (i, e) {
               if ($(e).get('0').attribs.cellpadding === '3') {
@@ -47,10 +54,11 @@ app.get('/scrape', function (req, res) {
               }
             });
 
+            var date, time, loc, type;
+            var json = { date : "", time : "", loc : "", type : "" };
+
             $(kids).each(function (i, e) {
               $(e).each(function(j, x) {
-                var date, time, loc, type;
-                var json = { date : "", time : "", loc : "", type : "" };
 
                 if ($(x).children().first().find('span').length != '0') {
                   json.date = $(x).children().first().find('span').text();
@@ -65,16 +73,28 @@ app.get('/scrape', function (req, res) {
                 }
 
                 self.crimes.push(json);
+                // console.log(self.crimes.length);
               });
             });
-
           }
-        });
 
+          finished();
+        });
       });
     }
-  });
 
+    function writeToFile() {
+      fs.writeFile('output.json', JSON.stringify(self.crimes, null, 4), function (err_) {
+        if (!err_) {
+          console.log('file written successfully!');
+        } else {
+          console.log('aw man');
+        }
+      });
+
+      res.send('check for output.json in your directory!');
+    }
+  });
 });
 
 
